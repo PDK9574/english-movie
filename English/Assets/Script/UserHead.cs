d7f23ea91d4cf6cbbff699d43be51925957ce797
EAS <97860858+Eason10756026@users.noreply.github.com>
2022-11-16 00:06:38 +0800

    更改頭像功能

---
+++ English/Assets/Script/UserHead.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+public class UserHead : MonoBehaviour
+{
+    public Image original;
+    public Sprite newSprite;
+    public Sprite newSprite2;
+    public Sprite newSprite3;
+    public Sprite newSprite4;
+    public int imageNumber;
+
+    public void Start()
+    {
+        imageNumber = PlayerPrefs.GetInt("imageNumber", 1);
+
+        if (imageNumber == 1)
+            original.sprite = newSprite;
+
+        if (imageNumber == 2)
+            original.sprite = newSprite2;
+
+        if (imageNumber == 3)
+
+            original.sprite = newSprite3;
+
+
+        if (imageNumber == 4)
+        {
+            original.sprite = newSprite4;
+            imageNumber = 0;
+        }
+    }
+
+    public void SetImage()
+    {
+
+
+
+        imageNumber++;
+
+
+        PlayerPrefs.SetInt("imageNumber", imageNumber);
+
+
+        if (imageNumber == 1)
+            original.sprite = newSprite;
+
+        if (imageNumber == 2)
+            original.sprite = newSprite2;
+
+        if (imageNumber == 3)
+
+            original.sprite = newSprite3;
+
+
+        if (imageNumber == 4)
+        {
+            original.sprite = newSprite4;
+            imageNumber = 0;
+        }
+
+    }
+
+
+
+}
